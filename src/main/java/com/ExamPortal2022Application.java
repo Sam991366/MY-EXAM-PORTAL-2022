02607427fc77b3570f503fd05ebf765b41d9fdf8
samarthladani133 <samarthladani133@gmail.com>
2022-08-08 14:37:38 +0530

    my first commit

---
+++ src/main/java/com/ExamPortal2022Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortal2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPortal2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ExamPortal2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPortal2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
